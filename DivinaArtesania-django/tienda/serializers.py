ee3050bf711bdb86b8a1c48de03d8977d3af2791
Benjamín Acevedo <be.acevedol@duocuc.cl>
2026-03-02 01:44:24 -0300

    Limpieza: eliminando archivos de otros proyectos y dejando solo portafolio

--- DivinaArtesania-django/tienda/serializers.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import serializers
-from .models import Producto  # Importa el modelo desde la misma carpeta 'tienda'
-
-class ProductoSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Producto
-        fields = '__all__'  # Esto incluye id, nombre, precio, stock, imagen, etc.
--- DivinaArtesania-ionic/src/environments/environment.prod.ts
+++
@@ -1,5 +0,0 @@
-export const environment = {
-  production: true,
-  apiUrl: 'https://TU-SERVIDOR-PRODUCCION.com/api/',  // ← Cambia esto cuando subas el proyecto
-  adminPin: '1234'                                      // ← Cambia esto antes de publicar
-};
